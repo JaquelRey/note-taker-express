a7e4668ebefc11a7c8b70905ce6f7accd45113ec
jaquel <jaquelrey@gmail.com>
2022-09-23 14:23:14 -0400

    forgot to track assets, filled out 404 page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 //require express
-
+const express = require('express');
 //import router
-
+const noteRouter = require('./tips')
 //create app with express
-
+const app = express();
 //use route
-
+app.use('/notes', noteRouter);
 //export
+module.exports = app;
